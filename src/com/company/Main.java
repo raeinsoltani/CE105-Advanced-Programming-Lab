46504038e13402bf604bab5317c5ac3f4b52f4cb
raeinsoltani <raeen.soltani@yahoo.com>
2022-02-15 17:50:34 +0330

    feat: added calMethod and finished the program.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,24 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please choose between mathematical functions + - * / :");
+        System.out.print("Please choose between mathematical functions + - * / : ");
         char function = scanner.next().charAt(0);
+        System.out.print("Please enter first number: ");
+        int var1 = scanner.nextInt();
+        System.out.print("Please enter second number: ");
+        int var2 = scanner.nextInt();
+        double res = Main.calMethod(function, var1, var2);
+        System.out.print("Result is: " + res);
+    }
+
+    static double calMethod(char function, int var1, int var2){
+        double res = 0;
+        switch (function){
+            case '+' -> res = var1 + var2;
+            case '-' -> res = var1 - var2;
+            case '*' -> res = var1 * var2;
+            case '/' -> res = 1.0* var1 / var2;
+        }
+        return res;
     }
 }
