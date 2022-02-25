c2b9607c1cb0b228420fd7aaaf06d41d2964982b
Raein Soltani <raeen.soltani@yahoo.com>
2022-02-25 11:59:30 +0330

    feat: Created Student Class fields and constructors.

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,19 @@
 package com.company;
 
 public class Student {
+    private String firstname;
+
+    private String lastname;
+
+    private int id; //id is set as an int unlike what the file has told to do
+
+    private double grade;
+
+    public Student(String firstname, String lastname, int id){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id;
+        grade = 0;
+    }
 
 }
