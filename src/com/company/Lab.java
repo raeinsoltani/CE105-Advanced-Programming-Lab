1b77566a6a2cde073f044320aa65436846553d51
Raein Soltani <raeen.soltani@yahoo.com>
2022-02-25 13:15:14 +0330

    created Fields for Lab Class

--- src/com/company/Lab.java
+++ src/com/company/Lab.java
@@ -1,5 +1,12 @@
 package com.company;
 
 public class Lab {
+    private Student[] students;
+    private String teacherName;
+    private String dayOfWeek; //Day of the week Lab is held at.
+    private int maxSize;
+    private int currentSize;
+    private double avgGrade;
+
 
 }
