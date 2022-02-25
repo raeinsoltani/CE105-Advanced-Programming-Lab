74a146449114510c50a5e4ac9782bc4f1a58d66f
Raein Soltani <raeen.soltani@yahoo.com>
2022-02-25 13:50:24 +0330

    feat: Created Constructor for Lab Class.

--- src/com/company/Lab.java
+++ src/com/company/Lab.java
@@ -2,11 +2,21 @@ package com.company;
 
 public class Lab {
     private Student[] students;
+
     private String teacherName;
+
     private String dayOfWeek; //Day of the week Lab is held at.
+
     private int maxSize;
+
     private int currentSize;
+
     private double avgGrade;
 
-
+    public Lab(String teacherName, String dayOfWeek, int maxSize){
+        this.teacherName = teacherName;
+        this.dayOfWeek = dayOfWeek;
+        this.maxSize = maxSize;
+        students = new Student[maxSize]; //Used for creating an array of students with max size.
+    }
 }
