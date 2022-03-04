79dd6509b4f798d59e000d34449889ceb3e16d30
Raein Soltani <raeen.soltani@yahoo.com>
2022-03-04 21:46:45 +0330

    feat: Added Address Class Methods and fields.

--- src/com/company/Address.java
+++ src/com/company/Address.java
@@ -1,5 +1,42 @@
 package com.company;
 
 public class Address {
+    private String zipcode;
 
+    private String country;
+
+    private String city;
+
+    public String toString(){
+        String res = "";
+        res += "ZipCode: " + zipcode;
+        res += "\n" + "Country: " + country;
+        res += "\n" + "City: " + city;
+        res += "\n";
+        return res;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
