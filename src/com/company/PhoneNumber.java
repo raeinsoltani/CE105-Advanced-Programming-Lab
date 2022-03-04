b2ba24f0744292ac3a0aac6f5c1ae09b467f2d2f
Raein Soltani <raeen.soltani@yahoo.com>
2022-03-04 22:00:29 +0330

    feat:Created PhoneNumber Methods and getter and setters.

--- src/com/company/PhoneNumber.java
+++ src/com/company/PhoneNumber.java
@@ -1,5 +1,27 @@
 package com.company;
 
 public class PhoneNumber {
+    private String countryCode;
 
+    private String number;
+
+    public String toString(){
+        return countryCode + number + "\n";
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
