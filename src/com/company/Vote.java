765a50f0d23958b13a0d58bece97dfa0518be39b
Raein Soltani <raeen.soltani@yahoo.com>
2022-03-10 16:11:12 +0330

    feat: Created equals and hashcodes.

--- src/com/company/Vote.java
+++ src/com/company/Vote.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Vote {
     private final Person voter;
 
@@ -16,5 +18,19 @@ public class Vote {
     public Vote(Person voter, String date) {
         this.voter = voter;
         this.date = date;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote = (Vote) o;
+        return Objects.equals(voter, vote.voter) && Objects.equals(date, vote.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(voter, date);
     }
 }
