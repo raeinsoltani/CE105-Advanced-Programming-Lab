2caaed24c50b6af71aec4d0ee9242f9e8427f1e0
Raein Soltani <raeen.soltani@yahoo.com>
2022-03-10 15:56:51 +0330

    feat: Created Person Class fields getter and setters

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -5,5 +5,19 @@ public class Person {
 
     private String lastName;
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
