248d2dfc9110dfc6220daf15590b526001c428f8
Raein Soltani <raeen.soltani@yahoo.com>
2022-03-10 18:45:52 +0330

    \feat: Created vote method.

--- src/com/company/Voting.java
+++ src/com/company/Voting.java
@@ -15,5 +15,23 @@ public class Voting {
 
     private ArrayList<Person> voters;
 
+    public ArrayList<String> getChoices(){
+        ArrayList<String> choices_String = new ArrayList<String>(choices.keySet());
+        return choices_String;
+    }
+
+    public void createChoice(String choice){
+        choices.put(choice, null);
+    }
+
+    public void vote(Person voter, ArrayList<String> voter_choices){
+        for (String voter_choices_i : voter_choices){
+            HashSet<Vote> voters = choices.get(voter_choices_i);
+            Vote vote = new Vote(voter, java.time.LocalDate.now().toString());
+            voters.add(vote);
+            choices.put(voter_choices_i, voters);
+        }
+    }
+
 
 }
