abf55d6e814ccf9e036d3201b532365b0b354d76
Raein Soltani <raeen.soltani@yahoo.com>
2022-06-27 11:48:41 +0430

    feat: added scence builder button methods!

--- src/main/java/com/example/workshop_11/HelloController.java
+++ src/main/java/com/example/workshop_11/HelloController.java
@@ -1,5 +1,101 @@
 package com.example.workshop_11;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class HelloController {
+
+    @FXML
+    private TextField text;
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void on0(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on2(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on3(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on4(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on5(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on6(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void on9(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAC(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDev(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEqual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMinus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onMul(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPlus(ActionEvent event) {
+
+    }
 
 }
