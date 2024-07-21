62acdca5ae2d1da032b10e78cb95ba652a80debd
Raein Soltani <raeen.soltani@yahoo.com>
2024-07-21 19:51:21 +0330

    Move workshop-11 content to workshop-11 directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.workshop_11 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.workshop_11 to javafx.fxml;
-    exports com.example.workshop_11;
-}
